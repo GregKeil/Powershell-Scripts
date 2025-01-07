26e7e29ffde4a1ac8cdb2757fdad3ca308fd4cc2
Greg K. <47342964+GregKeil@users.noreply.github.com>
2025-01-07 13:50:04 -0800

    Add files via upload

---
+++ Exe To B64.ps1
@@ -0,0 +1,14 @@
+﻿
+
+#Enter Full Filename & Path:
+$FileName = "C:\Users\DCO_Admin\Desktop\Projects\Win UF Installer\SANDBOX-2\Sysmon.exe"
+
+$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($FileName))
+
+#Enter Output Location Of Base64 String:
+echo $base64string > "C:\Users\DCO_Admin\Desktop\Projects\Win UF Installer\SANDBOX-2\Sysmon.txt"
+
+
+## Command To Decode An Output The File
+# $NewFileName = "FULL NAME & PATH"
+#[IO.File]::WriteAllBytes($NewFileName, [Convert]::FromBase64String($base64string))
---
+++ Testing Hidden Commands.ps1
@@ -0,0 +1,7 @@
+﻿$SHUTDOWN = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(""))
+
+$SYSTEM = "powershell.exe -EncodedCommand" 
+
+#$SHUTDOWN = "dwBoAG8AYQBtAGkA"
+
+cmd.exe /c "$SYSTEM $SHUTDOWN"
